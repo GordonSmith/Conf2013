04efef22d29a96fa7c521b75d8a6a893773600e8
Gordon Smith <gordon.smith@lexisnexis.com>
2013-08-19 12:36:59 +0100

    WIP
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- javascripts/d3.js
+++ javascripts/d3.js
@@ -188,6 +188,7 @@ define([
         },
         ForceDirected: function (_target) {
             var target = this.mixinTarget(_target);
+
             var color = d3.scale.category20();
             var force = d3.layout.force().charge(-120).linkDistance(30).size([target.width, target.height]);
             var svg = d3.select(target.domNode).append("svg").attr("width", target.width).attr("height", target.height).append("g");
@@ -226,6 +227,110 @@ define([
                 force.tick()
             }
             force.stop();
+        },
+        CoOccurrence: function (_target) {
+            var target = this.mixinTarget(_target);
+
+            function row(e) {
+                var t = d3.select(this).selectAll(".cell").data(e.filter(function (e) {
+                    return e.z
+                })).enter().append("rect").attr("class", "cell").attr("x", function (e) {
+                    return x(e.x)
+                }).attr("width", x.rangeBand()).attr("height", x.rangeBand()).style("fill-opacity", function (e) {
+                    return z(e.z)
+                }).style("fill", function (e) {
+                    return nodes[e.x].category == nodes[e.y].category ? c(nodes[e.x].category) : null
+                }).on("mouseover", mouseover).on("mouseout", mouseout)
+            }
+            function mouseover(e) {
+                d3.selectAll(".row text").classed("active", function (t, n) {
+                    return n == e.y
+                });
+                d3.selectAll(".column text").classed("active", function (t, n) {
+                    return n == e.x
+                })
+            }
+            function mouseout() {
+                d3.selectAll("text").classed("active", false)
+            }
+            function order(e) {
+                x.domain(orders[e]);
+                var t = svg.transition().duration(2500);
+                t.selectAll(".row").delay(function (e, t) {
+                    return x(t) * 4
+                }).attr("transform", function (e, t) {
+                    return "translate(0," + x(t) + ")"
+                }).selectAll(".cell").delay(function (e) {
+                    return x(e.x) * 4
+                }).attr("x", function (e) {
+                    return x(e.x)
+                });
+                t.selectAll(".column").delay(function (e, t) {
+                    return x(t) * 4
+                }).attr("transform", function (e, t) {
+                    return "translate(" + x(t) + ")rotate(-90)"
+                })
+            }
+            dojo.query("head").append("<style>text.active { fill: red; } </style>");
+            var margin = {
+                top: 80,
+                right: 0,
+                bottom: 20,
+                left: 100
+            };
+            target.width -= margin.left + margin.right;
+            target.height -= margin.top + margin.bottom;
+            var x = d3.scale.ordinal().rangeBands([0, target.width]), z = d3.scale.linear().domain([0, 4]).clamp(true), c = d3.scale.category10().domain(d3.range(10));
+            var svg = d3.select(target.domNode).append("svg").attr("width", target.width).attr("height", target.height + margin.top + margin.bottom).style("margin-left", 0 + "px").append("g").style("font", "10px sans-serif").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            var matrix = [];
+            var nodes = this.vertices;
+            var n = nodes.length;
+            nodes.forEach(function (e, t) {
+                e.index = t;
+                e.count = 0;
+                matrix[t] = d3.range(n).map(function (e) {
+                    return {
+                        x: e,
+                        y: t,
+                        z: 0
+                    }
+                })
+            });
+            this.edges.forEach(function (e) {
+                matrix[e.source.index][e.target.index].z += e.weight;
+                matrix[e.target.index][e.source.index].z += e.weight;
+                matrix[e.source.index][e.source.index].z += e.weight;
+                matrix[e.target.index][e.target.index].z += e.weight;
+                nodes[e.source.index].count += e.weight;
+                nodes[e.target.index].count += e.weight
+            });
+            var orders = {
+                name: d3.range(n).sort(function (e, t) {
+                    return d3.ascending(nodes[e].name, nodes[t].name)
+                }),
+                count: d3.range(n).sort(function (e, t) {
+                    return nodes[t].count - nodes[e].count
+                }),
+                category: d3.range(n).sort(function (e, t) {
+                    return nodes[t].category - nodes[e].category
+                })
+            };
+            x.domain(orders.category);
+            svg.append("rect").attr("width", target.width).attr("height", target.height).style("fill", "#eee");
+            var row = svg.selectAll(".row").data(matrix).enter().append("g").attr("class", "row").attr("transform", function (e, t) {
+                return "translate(0," + x(t) + ")"
+            }).each(row);
+            row.append("line").attr("x2", target.width).style("stroke", "#fff");
+            row.append("text").attr("x", -6).attr("y", x.rangeBand() / 2).attr("dy", ".32em").attr("text-anchor", "end").text(function (e, t) {
+                return nodes[t].name
+            });
+            var column = svg.selectAll(".column").data(matrix).enter().append("g").attr("class", "column").attr("transform", function (e, t) {
+                return "translate(" + x(t) + ")rotate(-90)"
+            });
+            column.append("line").attr("x1", -target.width).style("stroke", "#fff");
+            column.append("text").attr("x", 6).attr("y", x.rangeBand() / 2).attr("dy", ".32em").attr("text-anchor", "start").text(function (e, t) {
+                return nodes[t].name
+            });
         }
     });
     return {
